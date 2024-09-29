4907cc17330df3e628dd86c8f10fd5562e173aa0
annakulova <aakulovaa03@gmail.com>
2024-09-29 18:33:37 +0300

    create splash screen and footer at home page
    Signed-off-by: annakulova <aakulovaa03@gmail.com>

---
+++ app/src/main/java/com/aakulova/letsevent/HomeActivity.java
@@ -0,0 +1,52 @@
+package com.aakulova.letsevent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void goToNotices(View v) {
+        Intent intent = new Intent(this, NoticesActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToNews(View v) {
+        Intent intent = new Intent(this, NewsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSaved(View v) {
+        Intent intent = new Intent(this, SavedActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToChat(View v) {
+        Intent intent = new Intent(this, ChatActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToProfile(View v) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+}
